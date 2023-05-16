fa32ca94b264c2459ee36c2cea20ca6146ffe00c
miguel.dias <miguel.dias@retail-consult.com>
2023-05-16 11:18:49 +0100

    images

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
+
 const { body } = require('express-validator');
 
 const router = express.Router();
